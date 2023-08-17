2fdc21dab22d880fd4000e25593576160d202dc8
Victor Dibia <victordibia@microsoft.com>
2023-08-16 19:57:15 -0700

    prompt eng for recommender update

--- lida/components/viz/vizrecommender.py
+++ lida/components/viz/vizrecommender.py
@@ -8,12 +8,27 @@ from lida.datamodel import Goal, Summary
 
 
 system_prompt = """
-You are a helpful assistant highly skilled in recommending a DIVERSE set of visualizations as code. Your input is an example visualization code,  a summary of a dataset and an example visualization goal. Given this input, your task is to recommend an additional DIVERSE visualizations that a user may be interesting to a user. Consider different types of valid aggregations, chart types, and use different variables from the data summary. THE CODE YOU GENERATE MUST BE CORRECT AND FOLLOW VISUALIZATION BEST PRACTICES.
+You are a helpful assistant highly skilled in recommending a DIVERSE set of visualization code. Your input is an example visualization code,  a summary of a dataset and an example visualization goal. Given this input, your task is to recommend an additional DIVERSE visualizations that a user may be interested. Your output considers different types of valid aggregations, chart types, and uses different variables from the data summary. THE CODE YOU GENERATE MUST BE CORRECT AND FOLLOW VISUALIZATION BEST PRACTICES.
 
-Your output MUST be perfect JSON snippet in THE FORM OF A VALID JSON LIST without any additional explanation  e.g.,
+Your output MUST be a n code snippets separated by ******* (5 asterisks). For example
+
+```python
+# code snippet 1
+....
 ```
-[{"code": "import ...", "index":0}, .. {"code": "import ...", "index":1} ]
+*****
+
+```python
+# code snippet 2
+....
 ```
+
+```python
+# code snippet n
+....
+```
+
+Do not include any text or explanation or prose.
 """
 
 # refactor this to return n predictions ...
@@ -33,7 +48,7 @@ class VizRecommender(object):
             textgen_config: TextGenerationConfig,
             text_gen: TextGenerator,
             n=3,
-            library='altair'):
+            library='seaborn'):
         """Recommend a code spec based on existing visualization"""
 
         library_template, library_instructions = self.scaffold.get_template(Goal(
@@ -47,19 +62,15 @@ class VizRecommender(object):
             {"role": "system", "content": f"The dataset summary is : {summary}"},
             {"role": "system",
              "content":
-             f"An example visualization code is: {code}. You MUST use only the {library} library with the following instructions {library_instructions}. Each recommended visualization CODE MUST use the following template {library_template}."},
-            {"role": "user", "content": f"Now write code for {n} visualizations in the JSON list format. YOU MUST RETURN ONLY A JSON LIST"}
-        ]
+             f"An example visualization code is: ```{code}```. You MUST use only the {library} library. \n Each of your recommended code MUST ONLY MODIFY the content of the plot(data) method in the code above. \n"},
+            {"role": "system", "content": f"Recommend {n} (n=({n})) visualizations in the format specified. \n."}]
 
         textgen_config.messages = messages
         result: TextGenerationResponse = text_gen.generate(
             messages=messages, config=textgen_config)
         try:
-            json_string = clean_code_snippet(result.text[0]["content"])
-            result = json.loads(json_string)
-            if isinstance(result, dict):
-                result = [result]
-            result = [x["code"] for x in result]
+            snippets = result.text[0]["content"].split("*****")
+            result = [clean_code_snippet(x) for x in snippets]
         except json.decoder.JSONDecodeError:
             logger.info(
                 f"Error decoding JSON for generated visualization recommendations: {result.text[0]['content']}")
